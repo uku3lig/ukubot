0cc391263d34b60c21ff66b6cf92a402e5be7a17
Uku3lig <thelololomail@gmail.com>
2021-01-20 15:09:40 +0100

    Made core subsystems

---
+++ src/main/java/net/uku3lig/ukubot/subsystems/core/ReactionListener.java
@@ -0,0 +1,158 @@
+package net.uku3lig.ukubot.subsystems.core;
+
+import com.vdurmont.emoji.EmojiParser;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.events.message.guild.react.GuildMessageReactionAddEvent;
+import net.uku3lig.ukubot.core.Main;
+import net.uku3lig.ukubot.commands.IsSenderAllowed;
+import net.uku3lig.ukubot.subsystems.Subsystem;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.util.*;
+
+public class ReactionListener extends Subsystem {
+    @Override
+    public @NotNull String getName() {
+        return "Reaction Listener";
+    }
+
+    private final Message source;
+    private final Map<String, Runnable> actions;
+    private final IsSenderAllowed allowed;
+    private final int maxUses;
+    private int currentUses = 0;
+    private final Duration timeout;
+    private Timer timer = new Timer();
+
+    @Override
+    public void onGuildMessageReactionAdd(@NotNull GuildMessageReactionAddEvent event) {
+        if (!event.getMessageId().equals(source.getId())) return;
+        if (event.getReactionEmote().isEmote()) return;
+        if (event.getReactionEmote().isEmote())
+            if (!actions.containsKey(event.getReactionEmote().getEmote().getAsMention())) return;
+        else if (!actions.containsKey(event.getReactionEmote().getEmoji())) return;
+        if (actions.keySet().stream()
+                .noneMatch(emoji -> event.getReactionEmote().getEmoji().equals(emoji))) return;
+        if (!allowed.isAllowed(event.getMember())) return;
+
+        actions.get(event.getReactionEmote().isEmote() ?
+                event.getReactionEmote().getEmote().getAsMention() :
+                event.getReactionEmote().getEmoji()).run();
+        currentUses++;
+        timer = scheduleFromNow(timer, timeout);
+        if (currentUses >= maxUses) Main.getJda().removeEventListener(this);
+    }
+
+    private Timer scheduleFromNow(Timer t, Duration d) {
+        t.cancel();
+        t = new Timer();
+        ReactionListener instance = this;
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                source.getChannel().sendMessage("Cancelled due to timeout").queue();
+                Main.getJda().removeEventListener(instance);
+            }
+        }, Date.from(Instant.now().plus(d)));
+        return t;
+    }
+
+    //Protected to prevent user instantiation, but can still be used by reflections
+    private ReactionListener() {
+        source = null;
+        actions = Collections.emptyMap();
+        allowed = IsSenderAllowed.Default;
+        maxUses = 0;
+        timeout = Duration.ZERO;
+    }
+
+    private ReactionListener(Message source, Map<String, Runnable> actions, IsSenderAllowed allowed,
+                             int maxUses, Duration timeout) {
+        this.source = source;
+        this.actions = actions;
+        this.allowed = allowed;
+        this.maxUses = maxUses;
+        this.timeout = timeout;
+        this.timer = scheduleFromNow(timer, timeout);
+
+        this.actions.keySet().forEach(r -> this.source.addReaction(getReactionEmote(r)).queue());
+
+        Main.getJda().addEventListener(this);
+    }
+
+    private String getReactionEmote(String source) {
+        return String.join(":",
+                source.replace("<", "")
+                .replace(">", "")
+                .split(":"));
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static Builder yesNo(Runnable yes, Runnable no) {
+        return new Builder(Map.of(
+                EmojiParser.parseToUnicode(":white_check_mark:"), yes,
+                EmojiParser.parseToUnicode(":negative_squared_cross_mark:"), no));
+    }
+
+    public static Builder yesNo(Runnable yes, TextChannel c) {
+        return yesNo(yes, () -> c.sendMessage("ok canceled").queue());
+    }
+
+    public static class Builder {
+        private Message source = null;
+        private Map<String, Runnable> actions = null;
+        private IsSenderAllowed allowed = IsSenderAllowed.Default;
+        private int maxUses = 1;
+        private Duration timeout = Duration.ofMinutes(1);
+
+        public Builder(Map<String, Runnable> actions) {
+            this.actions = actions;
+        }
+
+        public Builder() {}
+
+        public Builder source(Message source) {
+            this.source = source;
+            return this;
+        }
+
+        /**
+         * Adds the reaction emojis, along with the action for each emoji. <br>
+         * <b>WARNING:</b> The emoji (discord native) MUST be in unicode, and the
+         * {@link net.dv8tion.jda.api.entities.Emote emotes} (user added) must be in a mention format (<code>&lt;:name:id></code>).
+         * @param actions The {@link Map map} containing all the emojis and actions.
+         * @return itself.
+         */
+        public Builder actions(Map<String, Runnable> actions) {
+            this.actions = actions;
+            return this;
+        }
+
+        public Builder allowed(IsSenderAllowed allowed) {
+            this.allowed = allowed;
+            return this;
+        }
+
+        public Builder maxUses(int maxUses) {
+            this.maxUses = maxUses;
+            return this;
+        }
+
+        public Builder timeout(Duration timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public void build() {
+            if (source == null || actions == null) throw new IllegalArgumentException("Source message or actions are not set!");
+            new ReactionListener(source, actions, allowed, maxUses, timeout);
+        }
+    }
+}
---
+++ src/main/java/net/uku3lig/ukubot/subsystems/core/SubCommandListener.java
@@ -0,0 +1,130 @@
+package net.uku3lig.ukubot.subsystems.core;
+
+import net.dv8tion.jda.api.entities.*;
+import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
+import net.dv8tion.jda.api.events.message.priv.PrivateMessageReceivedEvent;
+import net.uku3lig.ukubot.core.Main;
+import net.uku3lig.ukubot.subsystems.Subsystem;
+import org.jetbrains.annotations.NotNull;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.function.Function;
+
+public class SubCommandListener extends Subsystem {
+    @Override
+    public @NotNull String getName() {
+        return "Sub-command listener";
+    }
+
+    private final User sender;
+    private final MessageChannel channel;
+    private final Function<Message, Boolean> action;
+    private final Duration timeout;
+    private final boolean retry;
+    private Timer timer = new Timer();
+
+    @Override
+    public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
+        if (!(channel instanceof TextChannel)) return;
+        if (!sender.getId().equalsIgnoreCase(event.getAuthor().getId())) return;
+        if (!action.apply(event.getMessage()) && retry) timer = scheduleFromNow(timer, timeout);
+        else {
+            timer.cancel();
+            Main.getJda().removeEventListener(this);
+        }
+    }
+
+    @Override
+    public void onPrivateMessageReceived(@NotNull PrivateMessageReceivedEvent event) {
+        if (!(channel instanceof PrivateChannel)) return;
+        if (!sender.getId().equals(event.getAuthor().getId())) return;
+        if (!action.apply(event.getMessage()) && retry) timer = scheduleFromNow(timer, timeout);
+        else {
+            timer.cancel();
+            Main.getJda().removeEventListener(this);
+        }
+    }
+
+    private SubCommandListener() {
+        sender = null;
+        channel = null;
+        action = m -> false;
+        timeout = Duration.ZERO;
+        retry = false;
+    }
+
+    private SubCommandListener(User sender, MessageChannel channel, Function<Message, Boolean> action, Duration timeout, boolean retry) {
+        this.sender = sender;
+        this.channel = channel;
+        this.action = action;
+        this.timeout = timeout;
+        this.retry = retry;
+
+        timer = scheduleFromNow(timer, timeout);
+
+        System.out.println(channel.getClass().getSimpleName());
+
+        Main.getJda().addEventListener(this);
+    }
+
+    private Timer scheduleFromNow(Timer t, Duration d) {
+        t.cancel();
+        t = new Timer();
+        SubCommandListener instance = this;
+        t.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                channel.sendMessage("Cancelled due to timeout").queue();
+                Main.getJda().removeEventListener(instance);
+            }
+        }, Date.from(Instant.now().plus(d)));
+        return t;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static class Builder {
+        private User sender = null;
+        private MessageChannel channel = null;
+        private Function<Message, Boolean> action = null;
+        private Duration timeout = Duration.ofMinutes(1);
+        private boolean retry = false;
+
+        public Builder sender(User sender) {
+            this.sender = sender;
+            return this;
+        }
+
+        public Builder channel(MessageChannel channel) {
+            this.channel = channel;
+            return this;
+        }
+
+        public Builder timeout(Duration timeout) {
+            this.timeout = timeout;
+            return this;
+        }
+
+        public Builder retry(boolean retry) {
+            this.retry = retry;
+            return this;
+        }
+
+        public Builder action(Function<Message, Boolean> action) {
+            this.action = action;
+            return this;
+        }
+
+        public void build() {
+            if (channel == null || sender == null || action == null)
+                throw new IllegalArgumentException("action, channel and sender must NOT be null");
+            new SubCommandListener(sender, channel, action, timeout, retry);
+        }
+    }
+}
