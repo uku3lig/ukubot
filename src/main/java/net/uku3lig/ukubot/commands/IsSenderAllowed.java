7c419fabbbf751ddb3f703d03a5297888455fe7f
Uku3lig <thelololomail@gmail.com>
2021-01-20 15:09:26 +0100

    Added some constants to IsSenderAllowed

--- src/main/java/net/uku3lig/ukubot/commands/IsSenderAllowed.java
+++ src/main/java/net/uku3lig/ukubot/commands/IsSenderAllowed.java
@@ -1,11 +1,17 @@
 package net.uku3lig.ukubot.commands;
 
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 
 import java.util.function.Predicate;
 
 public enum IsSenderAllowed {
     Default(member -> true),
+    Moderator(member -> member.hasPermission(Permission.MESSAGE_MANAGE)),
+    Administrator(member -> member.hasPermission(Permission.ADMINISTRATOR)),
+    Friend(member -> member.getUser().getMutualGuilds().stream().map(Guild::getIdLong)
+            .anyMatch(id -> id == 796380718481408000L)),
     Uku(member -> member.getId().equals("319463560356823050"));
 
     private final Predicate<Member> p;
