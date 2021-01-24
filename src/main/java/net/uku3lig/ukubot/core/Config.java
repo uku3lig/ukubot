d500fb717be3f230189bac7dafa95aa6a9d9e88b
Uku3lig <thelololomail@gmail.com>
2021-01-24 12:53:01 +0100

    JPA-ed Config

--- src/main/java/net/uku3lig/ukubot/core/Config.java
+++ src/main/java/net/uku3lig/ukubot/core/Config.java
@@ -1,50 +1,98 @@
 package net.uku3lig.ukubot.core;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import net.dv8tion.jda.api.entities.Guild;
 import net.uku3lig.ukubot.commands.Command;
 import net.uku3lig.ukubot.commands.CommandAdapter;
 import net.uku3lig.ukubot.commands.IsSenderAllowed;
+import net.uku3lig.ukubot.hibernate.Database;
 
+import javax.persistence.*;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
-@Getter
+@Entity
+@Getter @Setter(AccessLevel.PRIVATE)
 public class Config {
-    public static final Map<Guild, Config> configs = new HashMap<>();
-    private static final Config defaultConfig = new Config(null);
+    private static Config defaultInstance;
 
-    @Setter
+    @Id
+    private long guildId;
+
+    @Transient
+    private Guild guild;
+
+    @Setter(AccessLevel.PUBLIC)
     private String prefix = "?";
-    private final Map<Class<? extends Command>, Duration> cooldowns = new HashMap<>();
-    private final Map<Class<? extends Command>, IsSenderAllowed> permissions = new HashMap<>();
+
+    @MapKeyColumn
+    @ElementCollection
+    private final Map<String, Duration> cooldowns = new HashMap<>();
+
+    @Transient
+    private final Map<String, IsSenderAllowed> permissions = new HashMap<>();
+
+    @MapKeyColumn
+    @ElementCollection
+    private final Map<String, String> persistentPermissions = new HashMap<>();
 
     protected Config(Guild g) {
         CommandAdapter.getCommands().forEach(c -> {
-            cooldowns.put(c.getClass(), c.cooldown());
-            permissions.put(c.getClass(), c.allowed());
+            cooldowns.put(c.getClass().getSimpleName(), c.cooldown());
+            permissions.put(c.getClass().getSimpleName(), c.allowed());
+            persistentPermissions.put(c.getClass().getSimpleName(), c.allowed().name());
         });
 
-        if (g != null) configs.put(g, this);
+        guildId = g.getIdLong();
+        Database.saveOrUpdate(this);
+    }
+
+    //JPA constructor
+    protected Config() {
+        Main.runWhenReady(jda -> guild = jda.getGuildById(guildId));
+        persistentPermissions.forEach((s, p) -> permissions.put(s, IsSenderAllowed.valueOf(p)));
     }
 
     public Config editCooldown(Class<? extends Command> command, Duration cooldown) {
-        if (cooldowns.get(command).isNegative() || permissions.get(command).equals(IsSenderAllowed.Uku))
+        if (cooldowns.get(command.getSimpleName()).isNegative() ||
+                permissions.get(command.getSimpleName()).equals(IsSenderAllowed.Uku))
             throw new SecurityException("Cannot change this command's cooldown");
-        cooldowns.put(command, cooldown);
+        cooldowns.put(command.getSimpleName(), cooldown);
         return this;
     }
 
     public Config editAllowed(Class<? extends Command> command, IsSenderAllowed allowed) {
-        if (permissions.get(command).equals(IsSenderAllowed.Uku))
+        if (permissions.get(command.getSimpleName()).equals(IsSenderAllowed.Uku))
             throw new SecurityException("Cannot change this command's allowed");
-        permissions.put(command, allowed);
+        permissions.put(command.getSimpleName(), allowed);
         return this;
     }
 
     public static Config newDefaultConfig(Guild g) {
         return new Config(g);
+    }
+
+    public static Optional<Config> getConfigByGuild(Guild g) {
+        return Database.getAll(Config.class).stream()
+                .filter(cfg -> cfg.guildId == g.getIdLong())
+                .findFirst();
+    }
+
+    static {
+        Main.runWhenReady(jda -> {
+            Set<Guild> cfgs = Database.getAll(Config.class).stream()
+                    .map(cfg -> cfg.guild).collect(Collectors.toSet());
+            Set<Guild> guilds = new HashSet<>(Main.getJda().getGuilds());
+            if (cfgs.size() != guilds.size()) {
+                guilds.removeAll(cfgs); //all guilds that aren't in config
+                cfgs.removeAll(Main.getJda().getGuilds()); //all configs that are linked to left guilds
+
+                guilds.stream().map(Config::new).forEach(Database::saveOrUpdate);
+                cfgs.stream().map(Config::new).forEach(Database::delete);
+            }
+        });
     }
 }
--- src/main/java/net/uku3lig/ukubot/subsystems/core/GuildHandler.java
+++ src/main/java/net/uku3lig/ukubot/subsystems/core/GuildHandler.java
@@ -1,11 +1,15 @@
 package net.uku3lig.ukubot.subsystems.core;
 
 import net.dv8tion.jda.api.events.guild.GuildJoinEvent;
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.uku3lig.ukubot.core.Config;
+import net.uku3lig.ukubot.hibernate.Database;
 import net.uku3lig.ukubot.subsystems.Subsystem;
 import org.jetbrains.annotations.NotNull;
 
-public class GuildJoinHandler extends Subsystem {
+import java.util.Optional;
+
+public class GuildHandler extends Subsystem {
     @Override
     public @NotNull String getName() {
         return "GuildJoinEvent handler";
@@ -14,5 +18,12 @@ public class GuildJoinHandler extends Subsystem {
     @Override
     public void onGuildJoin(@NotNull GuildJoinEvent event) {
         Config.newDefaultConfig(event.getGuild());
+    }
+
+    @Override
+    public void onGuildLeave(@NotNull GuildLeaveEvent event) {
+        Optional<Config> cfg = Config.getConfigByGuild(event.getGuild());
+        if (cfg.isEmpty()) return;
+        Database.delete(cfg.get());
     }
 }
