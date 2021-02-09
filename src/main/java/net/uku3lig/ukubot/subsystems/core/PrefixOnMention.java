0ced1d3021c7c01c8453b2ba70c33dc9c718d232
Uku3lig <thelololomail@gmail.com>
2021-02-09 15:52:46 +0100

    Fixed PrefixOnMention

--- src/main/java/net/uku3lig/ukubot/subsystems/core/PrefixOnMention.java
+++ src/main/java/net/uku3lig/ukubot/subsystems/core/PrefixOnMention.java
@@ -1,10 +1,14 @@
 package net.uku3lig.ukubot.subsystems.core;
 
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.uku3lig.ukubot.config.Config;
 import net.uku3lig.ukubot.core.Main;
 import net.uku3lig.ukubot.subsystems.Subsystem;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+import java.util.Objects;
 
 public class PrefixOnMention extends Subsystem {
     @Override
@@ -26,6 +30,10 @@ public class PrefixOnMention extends Subsystem {
     public void onGuildMessageReceived(@NotNull GuildMessageReceivedEvent event) {
         if (!event.getMessage().getMentionedMembers()
                 .contains(event.getGuild().getMember(Main.getJda().getSelfUser()))) return;
+        User bot = Main.getJda().getSelfUser();
+        String[] mentions = new String[] {bot.getAsMention(),
+                Objects.requireNonNull(event.getGuild().getMember(bot)).getAsMention()};
+        if (Arrays.stream(mentions).noneMatch(m -> m.equalsIgnoreCase(event.getMessage().getContentRaw()))) return;
         Config.getConfigByGuild(event.getGuild()).ifPresent(cfg ->
                 event.getChannel().sendMessage("My prefix here is `%s`".formatted(cfg.getPrefix())).queue());
     }
