f1548a2644bd002f21fe62cbc191b85f47febc96
Uku3lig <thelololomail@gmail.com>
2021-02-21 10:41:35 +0100

    equals and hashcode of Word

--- src/main/java/net/uku3lig/ukubot/hibernate/entities/Word.java
+++ src/main/java/net/uku3lig/ukubot/hibernate/entities/Word.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import net.uku3lig.ukubot.hibernate.Database;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "wordFrequency")
@@ -39,5 +40,18 @@ public class Word {
     public void increaseFrequency(long amount) {
         frequency += amount;
         Database.saveOrUpdate(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word1 = (Word) o;
+        return word.equals(word1.word);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(word);
     }
 }
