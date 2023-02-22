c48b41be47e15b564389867a6068ee591396fdac
uku <uku3lig@gmail.com>
2023-02-22 17:20:44 +0100

    add echo

---
+++ src/main/java/net/uku3lig/ukubot/command/EchoCommand.java
@@ -0,0 +1,34 @@
+package net.uku3lig.ukubot.command;
+
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.events.interaction.command.GenericCommandInteractionEvent;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionMapping;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.CommandData;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.utils.AllowedMentions;
+import net.uku3lig.ukubot.core.ICommand;
+
+import java.util.EnumSet;
+
+public class EchoCommand implements ICommand {
+    private static final EnumSet<Message.MentionType> allowed = EnumSet.complementOf(EnumSet.of(Message.MentionType.EVERYONE, Message.MentionType.ROLE, Message.MentionType.HERE));
+
+    @Override
+    public CommandData getCommandData() {
+        return Commands.slash("echo", "say things")
+                .addOption(OptionType.STRING, "text", "the thing to say", true)
+                .setDefaultPermissions(DefaultMemberPermissions.DISABLED);
+    }
+
+    @Override
+    public void onCommand(GenericCommandInteractionEvent event) {
+        OptionMapping mapping = event.getOption("text");
+        if (mapping == null) return;
+
+        event.getMessageChannel().sendMessage(mapping.getAsString()).allowedMentions(allowed)
+                .flatMap(m -> event.reply("sent!").setEphemeral(true))
+                .queue();
+    }
+}
