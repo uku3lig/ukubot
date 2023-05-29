3ac7cc071b9b67039d98054e66555140762195ef
uku <uku3lig@gmail.com>
2023-05-29 11:49:59 +0200

    gayge

--- src/main/java/net/uku3lig/ukubot/core/KiyoListener.java
+++ src/main/java/net/uku3lig/ukubot/core/KiyoListener.java
@@ -11,13 +11,18 @@ public class KiyoListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
         if (event.getAuthor().isBot() || event.getAuthor().isSystem()) return;
+        String content = event.getMessage().getContentRaw().toLowerCase(Locale.ROOT);
 
-        if (event.getMessage().getContentRaw().toLowerCase(Locale.ROOT).contains("kiyohime")) {
+        if (content.contains("kiyohime")) {
             event.getMessage().reply("<:kiyobean:739895868215263232>").mentionRepliedUser(false).queue();
         }
 
-        if (event.getMessage().getContentRaw().toLowerCase(Locale.ROOT).contains("uku3lig")) {
+        if (content.contains("uku3lig")) {
             event.getMessage().addReaction(Emoji.fromFormatted("<:uku:1007036728294527066>")).queue();
+        }
+
+        if (content.contains("gay")) {
+            event.getChannel().sendMessage("<:Gayge:1107308980994318356>").queue();
         }
     }
 }
